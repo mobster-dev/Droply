1ac83ecf3d6b0cf8e88b22d824cd2834af3089ec
Ryan Santos <ryan.santos@fast2mine.com>
2025-01-11 15:29:25 -0300

    translate examples

--- example_script.js
+++ example_script.js
@@ -5,17 +5,17 @@ function teste(key, label, selectedValues){
 document.addEventListener('DOMContentLoaded' , function(){
 
     // Posso passar como lista, onde os valores serao gerados automaticamente a partir do 0
-    //const tipo = ["Carro", "Moto", "Caminhão", "Ônibus"] 
+    //const tipo = ["car", "Motocycle", "Truck", "Bus"] 
 
     // Posso passar como objeto, onde os valores sao previamente definidos
-    const tipo = {
-        "0": "Carro",
-        "1": "Moto",
-        "2": "Caminhão",
-        "3": "Ônibus",
+    const type = {
+        "0": "Car",
+        "1": "Motocycle",
+        "2": "Truck",
+        "3": "Bus",
     }
     
-    const marca = {
+    const brand = {
         "0": ["Toyota", ["0"]],
         "1": ["Honda", ["0", "1"] ],
         "2": ["Ford", ["0", "2"] ],
@@ -23,23 +23,19 @@ document.addEventListener('DOMContentLoaded' , function(){
         "4": ["BMW", ["0"] ],
     }
     
-    const automovel = {
+    const automobile = {
         "0": { label: "Corolla", tipo: ["0"], marca: ["0"]}, 
         "1": { label: "Civic", tipo: ["0"], marca: ["1"]},
-        "2": { label: "Ranger", tipo: ["2"],  marca: ["2"]},
-        "3": { label: "S10", tipo: ["2"], marca: ["3"]},
+        "2": { label: "F-4000", tipo: ["2"],  marca: ["2"]},
+        "3": { label: "Silverado 2500HD", tipo: ["2"], marca: ["3"]},
         "4": { label: "X5", tipo: ["0"], marca: ["4"]},
         "5": { label: "Biz", tipo: ["1"], marca: ["1"]}, 
     }
 
     const droply = new Droply()
-    droply.CreateDropdown('TipoEquipamentos', tipo, undefined, teste)
-    droply.CreateDropdown('Frotas', marca)
-    droply.CreateDropdown('Equipamentos', automovel)
-    
-    document.getElementById('teste').addEventListener('click', () => {
-        console.log(droply.getNamedValues('Equipamentos'))
-    })
+    droply.CreateDropdown('Type', type, undefined, teste)
+    droply.CreateDropdown('Brand', brand)
+    droply.CreateDropdown('Equipment', automobile)
 
 })
 
